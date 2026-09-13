c17e2098cded94be573f95193df787355e3690e2
Amruta Tekale <atekale046@gmail.com>
2026-09-13 18:34:12 +0530

    fix: correct salary field of job schema

--- Backend/models/job.model.js
+++ Backend/models/job.model.js
@@ -18,7 +18,7 @@ const jobSchema = new mongoose.Schema(
         required: true,
     },
     salary: {
-        type: Number,
+        type: String,
         required: true,
     },
     jobType: {
