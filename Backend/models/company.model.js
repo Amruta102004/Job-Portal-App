38ad8b635a758484dc1710617046d38cc71c9aeb
Amruta Tekale <atekale046@gmail.com>
2026-09-13 16:27:14 +0530

    feat: add company model

--- Backend/models/company.model.js
+++ Backend/models/company.model.js
@@ -1,32 +1,37 @@
 import mongoose from "mongoose";
+
 const companySchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        unique: true,
     },
-    address: {
+
+    description: {
         type: String,
         required: true,
     },
+
     website: {
         type: String,
-        
     },
+
     location: {
         type: String,
-        
     },
+
     logo: {
-        type: String,  // URL for logo
+        type: String,
     },
-    userId: [{
+
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true,
-    }],
+    },
 
-
-}, {timestamps: true});
+}, { timestamps: true });
 
 const Company = mongoose.model("Company", companySchema);
+
 export default Company;
